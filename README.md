8fc72ac2b5c0bb798f842bd62cdbe29ec1f573e1
xradvan <xradvan@stuba.sk>
2020-02-29 17:03:13 +0100

    add CMake as a buildsystem instead of GNU Make
    Signed-off-by: xradvan <xradvan@stuba.sk>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DP Converter
 Extension to base field polynomial converter
+
+## Build and run
+This project uses CMake as a build system:
+1. Go to project directory
+2. Run ./configure.py
+3. cd .build
+4. make
+5. ./MQAnalyzer
---
+++ configure.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import os
+import sys
+import shutil
+import subprocess
+
+def usage():
+	print("Usage will be added later")
+
+# base dir
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# create build folder
+BUILDDIR = '.build'
+
+# clean build folder
+if len(sys.argv) > 1 and sys.argv[1] == 'clean':
+	if os.path.exists(BUILDDIR):
+		print('Removing {0} directory'.format(BUILDDIR))
+		shutil.rmtree(BUILDDIR, ignore_errors=True)
+	else:
+		print('Directory {0} does not exist. Run "configure.py" first.'.format(BUILDDIR))
+	exit(0)
+
+# check if build dir exists
+if os.path.exists(BUILDDIR):
+	print('Error: Build directory already exists. Run "configure.py clean" first.')
+	exit(1)
+
+print('Configuring .build directory')
+os.mkdir(BUILDDIR)
+
+# run cmake
+CMAKE = ['cmake', '../']
+if subprocess.call(CMAKE, cwd=BUILDDIR) == 0:
+	# print success
+	print('\n\nBuild directory created successfully. Now run "make" in {0} directory'.format(BUILDDIR))
+else:
+	print('\n\nError: Failed to create build directory. Run "configure.py clean" and guess why it happend...')
