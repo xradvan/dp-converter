b6a161d56eb4ab773ce7d0e087dcf80a0f443e49
xradvan <xradvan@stuba.sk>
2020-03-23 12:42:59 +0100

    converter: implement the 2nd conversion algorithm [Kipnis-Shamir]
    Signed-off-by: xradvan <xradvan@stuba.sk>

---
+++ converter/ToExtendedAlgorithm2.h
@@ -0,0 +1,27 @@
+/**
+ *@file ToExtendedAlgorithm2.h
+ *@author Bc. Peter Radvan
+ *@brief The second algorithm to convert base polynomial to extension field representation [Kipnis-Shamir]
+ *@version 0.1
+ *
+ *@copyright Copyright (c) 2020
+ *
+ */
+#pragma once
+
+#include "ToExtendedAlgInterface.h"
+#include <NTL/mat_GF2E.h>
+#include <NTL/vec_GF2E.h>
+#include <vector>
+
+class ToExtendedAlgrotihm2 : public ToExtendedAlgInterface
+{
+public:
+	ExtensionFieldPoly convert(const BasePolySet &toConvert) override;
+
+private:
+	NTL::vec_GF2EX createPartialPolyVec();
+
+	void transposeRS(NTL::vec_vec_GF2 &rightSides);
+	NTL::vec_GF2E getCol(const NTL::vec_vec_GF2 &rightSides_t, int index);
+};
