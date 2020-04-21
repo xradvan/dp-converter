fed3b0e0bf5124fed3bdace2089ba0412d8f7e97
xradvan <xradvan@stuba.sk>
2020-04-21 10:23:59 +0200

    analyzer: fix source loader to load single/multiple files
    Signed-off-by: xradvan <xradvan@stuba.sk>

--- analyzer/SourceLoader.cpp
+++ analyzer/SourceLoader.cpp
@@ -10,34 +10,54 @@
 Sources SourceLoader::loadSources(const Config &c)
 {
 	INFO("Loading souces from directory: " << c.inputBaseDir);
-	std::vector<std::string> entries;
-	for (const auto &entry : std::experimental::filesystem::directory_iterator(c.inputBaseDir)) {
-		entries.push_back(entry.path());
-	}
 
 	Sources result;
 	std::shared_ptr<FileInputInterface> input;
-
-	for (const auto &file : c.files) {
+	// Single file
+	if (c.inputBaseDir.empty()) {
+		PRINT(c.inputFile.name);
 		// Input format
-		if (file.format == C_FORMAT_DEFAULT)
+		if (c.inputFile.format == C_FORMAT_DEFAULT) {
 			input = std::make_shared<DefaultFileInput>();
-		else if (file.format == C_FORMAT_MQ)
+			input->setPath(c.inputFile.name);
+			result[c.inputFile.name].push_back({c.inputFile.name, input->getExtensionFieldPoly()});
+		}
+		else if (c.inputFile.format == C_FORMAT_MQ) {
 			input = std::make_shared<MQFileInput>();
+			input->setPath(c.inputFile.name);
+			result[c.inputFile.name].push_back({c.inputFile.name, input->getBasePolySet()});
+		}
 		else
-			throw MQAException{std::string{"Unknown format: "} + file.format};
+			throw MQAException{std::string{"Unknown format: "} + c.inputFile.format};
+	}
+	// Multiple files
+	else {
+		std::vector<std::string> entries;
+		for (const auto &entry : std::experimental::filesystem::directory_iterator(c.inputBaseDir)) {
+			entries.push_back(entry.path());
+		}
 
-		// Traverse subdirectories
-		for (const auto &entry : entries) {
-			auto path = entry + "/" + file.name;
-			auto fileName = path.substr(c.inputBaseDir.rfind("/"), std::string::npos);
-			input->setPath(path);
-			if (file.type == C_POLY_TYPE_BFP)
-				result[file.name].push_back({fileName, input->getBasePolySet()});
-			else if (file.type == C_POLY_TYPE_EFP)
-				result[file.name].push_back({fileName, input->getExtensionFieldPoly()});
+		for (const auto &file : c.files) {
+			// Input format
+			if (file.format == C_FORMAT_DEFAULT)
+				input = std::make_shared<DefaultFileInput>();
+			else if (file.format == C_FORMAT_MQ)
+				input = std::make_shared<MQFileInput>();
 			else
-				throw MQAException{std::string{"Unknown type: "} + file.type};
+				throw MQAException{std::string{"Unknown format: "} + file.format};
+
+			// Traverse subdirectories
+			for (const auto &entry : entries) {
+				auto path = entry + "/" + file.name;
+				auto fileName = path.substr(c.inputBaseDir.rfind("/"), std::string::npos);
+				input->setPath(path);
+				if (file.type == C_POLY_TYPE_BFP)
+					result[file.name].push_back({fileName, input->getBasePolySet()});
+				else if (file.type == C_POLY_TYPE_EFP)
+					result[file.name].push_back({fileName, input->getExtensionFieldPoly()});
+				else
+					throw MQAException{std::string{"Unknown type: "} + file.type};
+			}
 		}
 	}
 	return result;
